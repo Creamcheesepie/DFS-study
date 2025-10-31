7d837bbfe5718ad87ecfdfd971902e0114f04112
Creamcheesepie <gnldbs1004@naver.com>
2025-10-31 21:06:06 +0900

    DFS 노드 배치를 좀 더 보기 쉽게 재배치, 방문 순서도 알기 쉽게 함

--- src/day1/Dfs_Code_1.java
+++ src/day1/Dfs_Code_1.java
@@ -18,27 +18,28 @@ public class Dfs_Code_1 {
         Node n7 = new Node(7);
         Node n8 = new Node(8);
         root.addAdjacent(n1);
-        root.addAdjacent(n2);
-        n1.addAdjacent(n8);
-        n2.addAdjacent(n3);
-        n3.addAdjacent(n4);
+        n1.addAdjacent(n4);
         n4.addAdjacent(n5);
-        n5.addAdjacent(n6);
-        n2.addAdjacent(n7);
+
+        root.addAdjacent(n2);
+        n2.addAdjacent(n6);
+        n6.addAdjacent(n7);
+
+        root.addAdjacent(n3);
+        n3.addAdjacent(n8);
 
         search(root);
     }
 
     private static void search(Node root){
-        if(root == null) return;
-        root.visited();
-        for(Node node : root.adjacent){
-            if(!node.visited){
-                search(node);
+        if(root == null) return;// 넘겨받은 것이 없을 경우 돌아가기
+        root.visited(); // 현재 노드를 방문했다고 표시
+        for(Node node : root.adjacent){ // 노드에 인접한 노드들을 순차적으로 처리
+            if(!node.visited){ // 방문하지 않았다면
+                search(node); // 해당 노드를 정점으로 다시 DFS 진행
             }
         }
     }
-
-
-
+    // DFS 라서 해당 노드의 가장 깊은 곳까지 전개했다 돌아오는 것을 확인할 수 있음
+    // BFS였다면 해당 노드 주변을 전부 탐색하고 다음을 찾아보았을 것.
 }
