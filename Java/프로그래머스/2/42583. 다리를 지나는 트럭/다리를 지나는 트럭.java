5f71f5e524d04b4b0a1c6ae0d2d1e2394ec4130b
강휘윤 <126214324+Creamcheesepie@users.noreply.github.com>
2026-04-13 10:48:18 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 23.55 ms, Memory: 77.3 MB -BaekjoonHub

--- Java/프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ Java/프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -2,27 +2,27 @@ import java.util.*;
 
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int answer = 0;
-        Deque<Integer> deque = new ArrayDeque<>();
-        for(int i = 0; i<bridge_length; i++){
-            deque.offer(0);
-        }
+        int time = 0;
+        int totalWeight = 0;
+        Deque<Integer> bridge = new ArrayDeque<>();
+        for(int i = 0 ; i<bridge_length; i++) bridge.offer(0);
         
         int idx = 0;
-        int currentWeight = 0;
         while(idx < truck_weights.length){
-            answer++;
-            currentWeight -= deque.poll();
+            time++;
+            totalWeight -= bridge.poll();
             
-            if(currentWeight + truck_weights[idx] <= weight){
-                currentWeight += truck_weights[idx];
-                deque.offer(truck_weights[idx]);
+            int nt = truck_weights[idx];
+            
+            if(totalWeight + nt <= weight){
+                bridge.offer(nt);
+                totalWeight += nt;
                 idx++;
             } else{
-                deque.offer(0);
+                bridge.offer(0);
             }
         }
-        // 마지막 트럭이 출발점에 있으니 길이 만큼 더해줌(마지막 트럭이 나오는 시간)
-        return answer + bridge_length;
+        
+        return time + bridge_length;
     }
 }
