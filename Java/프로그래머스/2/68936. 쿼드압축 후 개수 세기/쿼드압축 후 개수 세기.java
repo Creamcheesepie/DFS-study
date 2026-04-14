7b822bd4b9b8adbb3da18c253388fb958a0ec0a4
강휘윤 <126214324+Creamcheesepie@users.noreply.github.com>
2026-04-14 09:21:37 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 42.97 ms, Memory: 114 MB -BaekjoonHub

--- Java/프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ Java/프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,33 +1,40 @@
 class Solution {
-    int rsZero = 0;
-    int rsOne = 0;
+    private int rsOne = 0;
+    private int rsZero = 0;
     
     public int[] solution(int[][] arr) {
-        sq(arr,0,0,arr.length);
-        return new int[] {rsZero, rsOne};
+        int[] answer = {};
+        int oneCnt = 0;
+        int zeroCnt = 0;
+        int n = arr.length;
+        // DP? 아니면 단순 시뮬레이션? DFS?
+        
+        sq(arr,0,0,n);
+        
+        
+        return new int[]{rsZero,rsOne};
     }
     
     private void sq(int[][] arr, int x,int y, int size){
-        int firstNumber = arr[x][y];
+        int firstNumb = arr[x][y];
         int zeroCnt = 0;
         int oneCnt = 0;
-        
-        for(int nx = x; nx<x + size; nx++){
-            for(int ny = y; ny<y + size; ny++){
-                if(arr[nx][ny] == 0) zeroCnt++;
-                if(arr[nx][ny] == 1) oneCnt++;
-                if(firstNumber != arr[nx][ny]) break;
+        for(int i = x; i < x + size; i++){
+            for(int j = y; j <y + size; j++){
+                if(arr[i][j] == 0) zeroCnt ++;
+                if(arr[i][j] == 1) oneCnt ++;
+                if(firstNumb != arr[i][j]) break;
             }
         }
-        
-        if(zeroCnt == 0) rsOne++;
+        if(zeroCnt == 0) rsOne ++;
         else if(oneCnt == 0) rsZero++;
         else {
             int nextSize = size/2;
-            sq(arr,x,y, nextSize);
-            sq(arr,x + nextSize,y, nextSize);
-            sq(arr,x,y + nextSize, nextSize);
-            sq(arr,x + nextSize,y + nextSize, nextSize);
+            sq(arr, x, y, nextSize);
+            sq(arr, x+nextSize, y,nextSize);
+            sq(arr, x , y + nextSize, nextSize);
+            sq(arr, x+nextSize, y+nextSize, nextSize);
         }
     }
+    
 }
